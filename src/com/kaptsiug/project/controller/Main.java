b93ec9b81fce66f92edf2a8535a58b0503edb334
Heorhi <heorhi.kaptsiug@gmail.com>
2021-06-04 00:14:08 +0300

    edit Main class

--- src/com/kaptsiug/project/controller/Main.java
+++ src/com/kaptsiug/project/controller/Main.java
@@ -1,5 +1,31 @@
 package com.kaptsiug.project.controller;
 
+import com.kaptsiug.project.logic.DogAction;
+import com.kaptsiug.project.logic.Eating;
+import com.kaptsiug.project.model.dog.Dog;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) {
+        // Build the area
+
+        List<Dog> dogs = new ArrayList<>();
+        Dog ken = new Dog("Ken", 1, true,true,true);
+        Dog ben = new Dog("Ben", 3, true,true,true);
+        Dog sam = new Dog("Sam", 9, true,true,true);
+
+        dogs.add(ken);
+        dogs.add(ben);
+        dogs.add(sam);
+
+        DogAction eating = new Eating();
+        for (Dog dog : dogs) {
+            eating.action(dog);
+        }
+
+    }
     
 }
